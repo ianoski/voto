6bccebfca06888e8f45a954463223d7cbeb02e86
Gabriela <gabriela_ianoski@estudante.sesisenai.org.br>
2024-02-08 11:06:35 -0300

    adicionado a classe e os estilos

---
+++ voto_class.js
@@ -0,0 +1,7 @@
+class Eleitor{
+    constructor(nome,idade,titulo){
+        this.nome = nome
+        this.idade = idade
+        this.titulo = titulo
+    }
+}
--- voto_main.js
+++ voto_main.js
@@ -6,6 +6,14 @@ verificar.addEventListener(`click`, ()=>{
     let idade = Number(document.getElementById('idade').value)
     let titulo = Number (document.getElementById('titulo').value)
 
-    console.log(nome,idade,titulo)
+    console.log("dados do formulário",nome,idade,titulo)
+
+    const cidadao = new Eleitor
+
+    cidadao.nome = nome
+    cidadao.idade = idade
+    cidadao.titulo = titulo
+
+    console.log("instância do objeto",cidadao)
     
 })
